c1af0ff530a2d7e63e8df1bac93245d6294e1a9b
samw1se97 <miodownick@gmail.com>
2025-01-12 16:58:03 +0200

    adding a contact me section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Separator from './components/Separator';
 import CvBtn from './components/CvBtn';
 import ProjectCard from './components/ProjectCard';
 import List from './components/List';
+import { MdWhatsapp } from 'react-icons/md';
+import { FaGoogle } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa6';
 function App() {
   // mySkills.map((skill) => console.log(skill));
   return (
@@ -39,7 +42,32 @@ function App() {
         </article>
 
         <Separator />
-        <CvBtn />
+        <article>
+          <h2 className='text_shdw name'>Download my CV </h2>
+          <CvBtn />
+        </article>
+        <Separator />
+
+        <article>
+          <h2 className='text_shdw name'>Contact me</h2>
+          <ul>
+            <li className='contact_li'>
+              <a href='/'>
+                <MdWhatsapp />
+              </a>
+            </li>
+            <li className='contact_li'>
+              <a href='/'>
+                <FaGoogle />
+              </a>
+            </li>
+            <li className='contact_li'>
+              <a href='/'>
+                <FaLinkedin />
+              </a>
+            </li>
+          </ul>
+        </article>
       </main>
     </>
   );
